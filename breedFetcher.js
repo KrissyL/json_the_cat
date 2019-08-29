08b231090c2fb0c25278a4f07cafc7ae01b627a0
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-29 22:59:52 +0000

    a program that fetchs a breed description from api.thecatapi.com

---
+++ breedFetcher.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const breedName = process.argv.slice(2);
+
+const getBreed = (breedName, callback) => {
+  const address = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+    
+  request(address, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      callback('Breed does not exist');
+      return;
+    } else {
+      const description = data[0].description;
+      callback(null, description);
+    }
+  });
+
+};
+const callback = function(err, description) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(description);
+  }
+};
+
+getBreed(breedName, callback);
